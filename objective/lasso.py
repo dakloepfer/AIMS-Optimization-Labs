380e6e90df7e9add2f0b4fd65c9048a1b99a9aa6
Dominik Kloepfer <dominikkloepfer@dhcp-90.wolf.ox.ac.uk>
2020-10-27 14:51:37 +0000

    Implemented SmoothedLasso Objective

--- objective/lasso.py
+++ objective/lasso.py
@@ -20,6 +20,8 @@ class Lasso(Objective):
 class Lasso_subGradient(Lasso):
     def task_error(self, w, x, y):
         self._validate_inputs(w, x, y)
+
+        # Compute mean squared error
         preds = torch.mm(x, w).squeeze()
         error = torch.mean(torch.square(preds - y))
         return error
@@ -31,7 +33,6 @@ class Lasso_subGradient(Lasso):
         # Compute objective value
         obj = self.task_error(w, x, y) + 0.5 * mu * torch.norm(w, p=1)
         # compute subgradient
-        n = x.size(0)
         error = torch.mm(x, w).squeeze() - y
         dw = 2 * torch.mean(x.transpose(0, 1) * error, dim=1, keepdim=True) + 0.5 * mu * torch.sign(w)
         return {'obj': obj, 'dw': dw}
@@ -40,8 +41,10 @@ class Lasso_subGradient(Lasso):
 class SmoothedLasso_Gradient(Lasso):
     def task_error(self, w, x, y):
         self._validate_inputs(w, x, y)
-        # TODO: Compute mean squared error
-        error = None
+
+        # Compute mean squared error
+        preds = torch.mm(x, w).squeeze()
+        error = torch.mean(torch.square(preds - y))
         return error
 
     def oracle(self, w, x, y):
@@ -50,8 +53,9 @@ class SmoothedLasso_Gradient(Lasso):
         mu = self.hparams.mu
         # temperature parameter
         temp = self.hparams.temp
-        # TODO: Compute objective value
-        obj = None
-        # TODO: compute gradient
-        dw = None
+        # Compute objective value
+        obj = self.task_error(w, x, y) + 0.5 * mu * temp * torch.sum(torch.log(2 * torch.cosh(w / temp)))
+        # compute gradient
+        error = torch.mm(x, w).squeeze() - y
+        dw = 2 * torch.mean(x.transpose(0, 1) * error, dim=1, keepdim=True) + 0.5 * mu * torch.tanh(w / temp)
         return {'obj': obj, 'dw': dw}
