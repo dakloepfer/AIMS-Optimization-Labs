c37be82d59d14d0292dec7f36250dcd68d067f18
Dominik Kloepfer <dominikkloepfer@dhcp-90.wolf.ox.ac.uk>
2020-10-28 15:49:50 +0000

    implemented (unvectorized) logistic regression

--- objective/logistic.py
+++ objective/logistic.py
@@ -16,16 +16,27 @@ class Logistic_Gradient(Objective):
 
     def task_error(self, w, x, y):
         self._validate_inputs(w, x, y)
-        # TODO: Compute cross entropy prediction error
-        error = None
+        # Compute prediction error (fraction of misclassified samples) 
+        scores = torch.mm(x, w)
+        _, preds = scores.max(dim=1)
+        mistakes = (preds != y).float()
+        error = mistakes.mean()
+
         return error
 
     def oracle(self, w, x, y):
         self._validate_inputs(w, x, y)
         # regularization hyper-parameter
         mu = self.hparams.mu
-        # TODO: Compute objective value
-        obj = None
-        # TODO: compute gradient
-        dw = None
+        # Compute objective value
+        obj = torch.mean(torch.log(torch.sum(torch.exp(torch.mm(x, w)), dim=1)) - torch.diag(torch.mm(x, w[:, y])))
+        obj += 0.5 * mu * torch.square(torch.norm(w))
+        
+        # compute gradient
+        dw = torch.zeros_like(w)
+        for i in range(0, w.shape[1]):
+            dwi = torch.mean(x * (torch.exp(torch.mv(x, w[:, i])) / torch.sum(torch.exp(torch.mm(x, w)), dim=1))[:, None] - torch.tensor(i == y).float()[:, None] * x, dim=0)
+            dw[:, i] = dwi
+
+        dw += mu * w
         return {'obj': obj, 'dw': dw}
